afdea56067948ff591c41649ad9f349ff7afce39
Muhammet Şükrü Kalpaklıoğlu <77413367+mskalpaklioglu@users.noreply.github.com>
2023-08-16 14:08:17 +0300

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatListModule} from '@angular/material/list';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatTableModule} from '@angular/material/table';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-   
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
-import { FiltersComponent } from './pages/home/components/filters/filters.component';
 import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
+import { FiltersComponent } from './pages/home/components/filters/filters.component';
+import { HeaderComponent } from './components/header/header.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { CartService } from './services/cart.service';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreService } from './services/store.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     HomeComponent,
     ProductsHeaderComponent,
-    FiltersComponent,
     ProductBoxComponent,
-    CartComponent
+    FiltersComponent,
+    HeaderComponent,
+    CartComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     MatSidenavModule,
     MatGridListModule,
     MatMenuModule,
@@ -45,13 +49,16 @@ import { CartComponent } from './pages/cart/cart.component';
     MatCardModule,
     MatIconModule,
     MatExpansionModule,
+    MatTreeModule,
     MatListModule,
     MatToolbarModule,
     MatTableModule,
     MatBadgeModule,
     MatSnackBarModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [CartService, StoreService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -1,21 +1,34 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StoreService } from 'src/app/services/store.service';
+import { Component, EventEmitter, OnInit, Output, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-filters',
   templateUrl:'filters.component.html',
 
 })
-export class FiltersComponent implements OnInit{
+export class FiltersComponent implements OnInit , OnDestroy{
   @Output() showCategory=new EventEmitter<string>();
-  categories=['shoes','sports']
+  categoriesSubscription: Subscription | undefined;
+  categories:Array<string> | undefined;
 
-  constructor() { }
+  constructor(private storeService: StoreService) { }
   
   ngOnInit(): void {
+    this.categoriesSubscription = this.storeService.getAllCategories()
+      .subscribe((response) => {
+        this.categories =response;
+      });
     
   }
 
   onShowCategory(category: string):void{
     this.showCategory.emit(category);
+  }
+
+  ngOnDestroy(): void {
+    if(this.categoriesSubscription) {
+      this.categoriesSubscription.unsubscribe();
+    }
   }
 }
